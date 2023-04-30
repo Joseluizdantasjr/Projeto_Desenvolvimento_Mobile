7a197f91823214ec44007d80cd38faa76e01fd4f
José Luiz Júnior <30448740+Joseluizdantasjr@users.noreply.github.com>
2023-04-30 16:29:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import CardFilme from './src/components/CardFilme';
+
+export default function App() {
+  
+  let [filmes, setFilmes] = useState([]);
+
+  const baseURL = 'https://api.otaviolube.com/api/filmes?populate=*';
+
+  useEffect(function(){
+    fetch(baseURL)
+      .then(data => data.json())
+      .then(objeto => {
+        console.log(objeto);
+        setFilmes(objeto.data);
+      }) 
+  }, []);
+  return (
+<SafeAreaView style={styles.container}>
+      <ScrollView horizontal>
+        { filmes.length > 0 ? filmes.map(filme => 
+        <CardFilme key={filme.id} filme={filme.attributes}/>) : 
+        <Text>Carregando ...</Text>}
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+});
